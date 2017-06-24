c4318982ece87b4a1c569615393db29ec96af9bd
EclipseXuLu <249048056@qq.com>
2017-06-24 21:07:31 +0800

    finish NLP word2vec training model

--- DataHouse/ml/liepin_analysis.py
+++ DataHouse/ml/liepin_analysis.py
@@ -4,6 +4,7 @@ Data mining and NLP task with liepin job detailed description corpus.
 import logging
 import os
 
+import gensim
 from gensim import corpora, models, similarities
 import jieba
 import jieba.analyse
@@ -11,6 +12,7 @@ import jieba.analyse
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 STOPWORDS_FILE = 'stopwords.txt'
 USER_DICT = 'userdict.txt'
+DOCUMENT_DIR = '/home/lucasx/PycharmProjects/DataHouse/DataSet/liepin/'
 
 
 def main(documents):
@@ -19,20 +21,20 @@ def main(documents):
     dictionary = corpora.Dictionary(texts)
 
     class MyCorpus(object):
-        def __iter__(self):
-            with open('/home/lucasx/PycharmProjects/DataHouse/DataSet/liepin/191938818.txt', mode='rt',
-                      encoding='UTF-8') as f:
-                # assume there's one document per line, tokens separated by whitespace
-                yield dictionary.doc2bow(cut_words(''.join(f.readlines())))
+        def __init__(self, dirname):
+            self.dirname = dirname
 
-    print(dictionary.doc2bow(
-        cut_words(read_document_from_text('/home/lucasx/PycharmProjects/DataHouse/DataSet/liepin/191938818.txt'))))
-    print('===========================================')
-    corpus_memory_friendly = MyCorpus()
-    tfidf = models.TfidfModel(corpus_memory_friendly)  # step 1 -- initialize a model
-    corpus_tfidf = tfidf[corpus_memory_friendly]
-    for doc in corpus_tfidf:
-        print(doc)
+        def __iter__(self):
+            for fname in os.listdir(self.dirname):
+                with open(os.path.join(self.dirname, fname), mode='rt',
+                          encoding='UTF-8') as f:
+                    yield cut_words(''.join(f.readlines()))
+
+    sentences = MyCorpus(DOCUMENT_DIR)
+    model = gensim.models.Word2Vec(sentences=sentences, iter=5, min_count=3, size=100, workers=2)
+    model.build_vocab(sentences=sentences)
+    model.train(sentences=sentences)
+    model.accuracy('/tmp/questions-words.txt')
 
 
 def get_stopwords(stopwords_filepath):
@@ -82,6 +84,5 @@ def cut_words(document):
 
 
 if __name__ == '__main__':
-    document_dir = '/home/lucasx/PycharmProjects/DataHouse/DataSet/liepin/'
-    documents = [read_document_from_text(os.path.join(document_dir, _)) for _ in os.listdir(document_dir)]
+    documents = [read_document_from_text(os.path.join(DOCUMENT_DIR, _)) for _ in os.listdir(DOCUMENT_DIR)]
     main(documents)
