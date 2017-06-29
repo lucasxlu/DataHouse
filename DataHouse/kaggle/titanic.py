b719de9d4aabe701983738a7b669c7e205eb4622
EclipseXuLu <249048056@qq.com>
2017-06-29 10:59:34 +0800

    V1.4.1_ALPHA finished --add Kaggle module

--- DataHouse/kaggle/titanic.py
+++ DataHouse/kaggle/titanic.py
@@ -4,6 +4,7 @@ Titanic survive prediction
 from sklearn.feature_selection import VarianceThreshold
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2
+from sklearn.neural_network import MLPClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import svm
 from sklearn.neighbors.nearest_centroid import NearestCentroid
@@ -65,7 +66,11 @@ def load_test_data(csv_filepath):
 
 
 if __name__ == '__main__':
+    percentage = 0.9
     X_train, y_train = load_training_set(TRAIN_CSV)
+    X_train, y_train = X_train[0: int(len(X_train) * percentage)], y_train[0: int(len(y_train) * percentage)]
+    X_val, y_val = X_train[int(len(X_train) * percentage): len(X_train)], y_train[
+                                                                          int(len(y_train) * percentage): len(y_train)]
     print('loading data successfully~')
     print('=' * 100)
     X_test = load_test_data(TEST_CSV)
@@ -75,32 +80,38 @@ if __name__ == '__main__':
     X_train = sel.fit_transform(X_train)
 
     print('start launching Random Forest Classifier......')
-    clf = RandomForestClassifier(n_estimators=10)
-    clf.fit(X_train, y_train)
-    y_predict = clf.predict(X_test)
+    rf = RandomForestClassifier(n_estimators=10)
+    rf.fit(X_train, y_train)
+    print('finish launching Random Forest Classifier, the test accuracy is {:.5%}'.format(rf.score(X_val, y_val)))
+    rf_predict = rf.predict(X_test)
 
-    dataframe = pd.DataFrame(pd.read_csv(TEST_CSV)['PassengerId'].tolist(), y_predict)
-    # dataframe.to_csv('~/gender_submission.csv')
-    print('finish launching Random Forest Classifier, the test accuracy is {:.5%}'.format(clf.score(X_test, y_test)))
-
-    """
     print('=' * 100)
     print('start launching SVM Classifier......')
     svm = svm.SVC()
     svm.fit(X_train, y_train)
-    svm.predict(X_test)
-    print('finish launching SVM Classifier, the test accuracy is {:.5%}'.format(svm.score(X_test, y_test)))
+    print('finish launching SVM Classifier, the test accuracy is {:.5%}'.format(svm.score(X_val, y_val)))
+    svm_predict = svm.predict(X_test)
 
     print('=' * 100)
     print('start launching KNN Classifier......')
     knn = NearestCentroid()
     knn.fit(X_train, y_train)
+    print('finish launching KNN Classifier, the test accuracy is {:.5%}'.format(knn.score(X_val, y_val)))
     knn.predict(X_test)
-    print('finish launching KNN Classifier, the test accuracy is {:.5%}'.format(knn.score(X_test, y_test)))
 
     print('=' * 100)
     print('start launching Decision Tree Classifier......')
     dtree = tree.DecisionTreeClassifier()
     dtree.fit(X_train, y_train)
-    print('finish launching Decision Tree Classifier, the test accuracy is {:.5%}'.format(dtree.score(X_test, y_test)))
-    """
+    print('finish launching Decision Tree Classifier, the test accuracy is {:.5%}'.format(dtree.score(X_val, y_val)))
+    dtree_predict = dtree.predict(X_test)
+
+    print('=' * 100)
+    print('start launching MLP Classifier......')
+    mlp = MLPClassifier(solver='lbfgs', alpha=1e-4, hidden_layer_sizes=(7, 2), random_state=1)
+    mlp.fit(X_train, y_train)
+    print('finish launching MLP Classifier, the test accuracy is {:.5%}'.format(mlp.score(X_val, y_val)))
+    mlp_predict = mlp.predict(X_test)
+
+    dataframe = pd.DataFrame(pd.read_csv(TEST_CSV)['PassengerId'].tolist(), dtree_predict)
+    dataframe.to_csv('~/gender_submission.csv')
