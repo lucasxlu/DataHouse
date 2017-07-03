aeb6fa7a3539962d9e52f20dbb29ab9fae326328
EclipseXuLu <249048056@qq.com>
2017-07-03 15:54:44 +0800

    V1.5.1_ALPHA finished --redefine API

--- DataHouse/textclassify/sae_rf.py
+++ DataHouse/textclassify/sae_rf.py
@@ -12,25 +12,25 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn import tree
 from sklearn.neighbors.nearest_centroid import NearestCentroid
 
-# TEXT_DIR = '/home/lucasx/Desktop/corpus_6_4000/'
+TEXT_DIR = '/home/lucasx/Desktop/corpus_6_4000/'
 # TEXT_DIR = '/home/lucasx/Desktop/20_newsgroups/'
-TEXT_DIR = '/home/lucasx/Desktop/FudanNLPCorpus/'
-FEATURE_NUM = 20000
+# TEXT_DIR = '/home/lucasx/Desktop/FudanNLPCorpus/'
+FEATURE_NUM = 30000
 STOPWORDS_FILE = 'stopwords.txt'
 USER_DICT = 'userdict.txt'
 
 
-def get_all_categories(dir_):
-    category_list = []
-    for _ in os.listdir(dir_):
-        category = _.split('_')[0].strip()
-        if category not in category_list:
-            category_list.append(category)
+def init_train_and_test_dataset(dir_):
+    def get_all_categories(dir_):
+        category_list = []
+        for _ in os.listdir(dir_):
+            category = _.split('_')[0].strip()
+            if category not in category_list:
+                category_list.append(category)
 
-    return category_list
+        return category_list
 
-
-def init_train_and_test_dataset(dir_, category_list):
+    category_list = get_all_categories(dir_)
     training_data_ratio = 0.7
     dataset = {
         'name': 'dataset'
@@ -61,7 +61,22 @@ def init_train_and_test_dataset(dir_, category_list):
         training_set[num] = dataset[num][0: int(len(dataset[num]) * training_data_ratio)]
         test_set[num] = dataset[num][int(len(dataset[num]) * training_data_ratio): len(dataset[num])]
 
-    return training_set, test_set
+    training_text = []
+    training_label = []
+    test_text = []
+    test_label = []
+    for label, filepath_list in training_set.items():
+        training_text += [read_document_from_text(_) for _ in filepath_list]
+        training_label += [label for _ in range(len(filepath_list))]
+
+    for label, filepath_list in test_set.items():
+        test_text += [read_document_from_text(_) for _ in filepath_list]
+        test_label += [label for _ in range(len(filepath_list))]
+
+    training_X = documents_to_tfidf_vec(training_text)
+    test_X = documents_to_tfidf_vec(test_text)
+
+    return training_X, training_label, test_X, test_label
 
 
 def init_20groups_data(base_dir):
@@ -89,7 +104,10 @@ def init_20groups_data(base_dir):
                        files_in_category[int(len(files_in_category) * 0.8): len(files_in_category)]]
         test_label += [label_and_num[_] for i in range(len(files_in_category) - int(len(files_in_category) * 0.8))]
 
-    return training_set, training_label, test_set, test_label
+    train_X = documents_to_tfidf_vec(training_set)
+    test_X = documents_to_tfidf_vec(test_set)
+
+    return train_X, training_label, test_X, test_label
 
 
 def get_stopwords(stopwords_filepath):
@@ -135,24 +153,7 @@ def read_document_from_text(text_filepath):
 
 
 if __name__ == '__main__':
-    """
-    category_list = get_all_categories(TEXT_DIR)
-    training_set, test_set = init_train_and_test_dataset(TEXT_DIR, category_list)
-
-    training_text = []
-    training_label = []
-    test_text = []
-    test_label = []
-    for label, filepath_list in training_set.items():
-        training_text += [read_document_from_text(_) for _ in filepath_list]
-        training_label += [label for _ in range(len(filepath_list))]
-
-    for label, filepath_list in test_set.items():
-        test_text += [read_document_from_text(_) for _ in filepath_list]
-        test_label += [label for _ in range(len(filepath_list))]
-
-    train_X = documents_to_tfidf_vec(training_text)
-    test_X = documents_to_tfidf_vec(test_text)
+    train_X, training_label, test_X, test_label = init_train_and_test_dataset(TEXT_DIR)
 
     print('=' * 100)
     print('start launching MLP Classifier......')
@@ -172,14 +173,14 @@ if __name__ == '__main__':
     knn = NearestCentroid()
     knn.fit(train_X, training_label)
     print('finish launching KNN Classifier, the test accuracy is {:.5%}'.format(knn.score(test_X, test_label)))
-    """
 
-    training_set, training_label, test_set, test_label = init_20groups_data(TEXT_DIR)
-    train_X = documents_to_tfidf_vec(training_set)
-    test_X = documents_to_tfidf_vec(test_set)
+"""
+    train_X, training_label, test_X, test_label = init_20groups_data(TEXT_DIR)
+    
 
     print('=' * 100)
     print('start launching MLP Classifier......')
     mlp = MLPClassifier(solver='lbfgs', alpha=1e-4, hidden_layer_sizes=(50, 30, 20, 20), random_state=1)
     mlp.fit(train_X, training_label)
     print('finish launching MLP Classifier, the test accuracy is {:.5%}'.format(mlp.score(test_X, test_label)))
+"""
