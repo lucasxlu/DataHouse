99ed5ca68d529ca2ff7e9e688c71be19a82d029a
EclipseXuLu <249048056@qq.com>
2018-01-20 17:06:21 +0800

    --update ZhihuLiveDB --API user crawler not finished yet~

--- DataHouse/zhihu/zhihu_live_regressor.py
+++ DataHouse/zhihu/zhihu_live_regressor.py
@@ -6,7 +6,7 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error
 
 
 def split_train_test(excel_path, test_ratio):
-    df = pd.read_excel(excel_path, sheetname="ZhihuLive").fillna(value=0)
+    df = pd.read_excel(excel_path, sheetname="Preprocessed").fillna(value=0)
     df = df[df['review_score'] > 0]
     print(df.describe())
     print("*" * 100)
@@ -35,12 +35,15 @@ def train_and_test_model(train, test, train_Y, test_Y):
     #                   ('linear', LinearRegression(fit_intercept=False))])
     model = RidgeCV(alphas=[_ * 0.1 for _ in range(1, 1000, 1)])
     model.fit(train, train_Y)
-    mae_lr = round(mean_absolute_error(test_Y, model.predict(test)), 4)
-    rmse_lr = round(np.math.sqrt(mean_squared_error(test_Y, model.predict(test))), 4)
+    predicted_score = model.predict(test)
+    mae_lr = round(mean_absolute_error(test_Y, predicted_score), 4)
+    rmse_lr = round(np.math.sqrt(mean_squared_error(test_Y, predicted_score)), 4)
+    pc = round(np.corrcoef(test_Y, predicted_score)[0, 1], 4)
     print('===============The Mean Absolute Error of Lasso Regression Model is {0}===================='.format(mae_lr))
     print('===============The Root Mean Square Error of Linear Model is {0}===================='.format(rmse_lr))
+    print('===============The Pearson Correlation of Model is {0}===================='.format(pc))
 
 
 if __name__ == '__main__':
-    train_set, test_set, train_label, test_label = split_train_test("D:/ZhihuLive.xlsx", 0.2)
+    train_set, test_set, train_label, test_label = split_train_test("./ZhihuLiveDB.xlsx", 0.2)
     train_and_test_model(train_set, test_set, train_label, test_label)
