25db6e9635e585a821bc573d9adcc176599d2cf4
LucasX <xulu0620@gmail.com>
2019-02-24 16:13:13 +0800

    add jd

--- DataHouse/jd/jd_goods_comments_spider.py
+++ DataHouse/jd/jd_goods_comments_spider.py
@@ -1,6 +1,8 @@
 import json
 import os
 import sys
+import time
+import random
 
 import requests
 
@@ -9,6 +11,11 @@ from DataHouse.jd.db_utils import insert_item
 
 
 def crawl_comments_by_good_id(good_id):
+    """
+    crawl goods comments by good_id
+    :param good_id:
+    :return:
+    """
     headers = {
         'DNT': '1',
         'Referer': 'https://item.jd.com/%d.html' % good_id,
@@ -21,16 +28,21 @@ def crawl_comments_by_good_id(good_id):
         url = 'https://sclub.jd.com/comment/productPageComments.action?callback=fetchJSON_comment98vv1739&productId={0}' \
               '&score=0&sortType=5&page={1}&pageSize=10&isShadowSku=0&fold=1'.format(good_id, page)
 
-        response = requests.get(url, headers=headers)
+        try:
+            response = requests.get(url, headers=headers)
 
-        if response.status_code == 200:
-            js = json.loads(response.text.replace('fetchJSON_comment98vv1739', '')[1: -2], encoding='utf-8')
-            print(js)
-            insert_item(js)
-        else:
-            print('Error!!! {0}'.format(response.status_code))
+            if response.status_code == 200:
+                js = json.loads(response.text.replace('fetchJSON_comment98vv1739', '')[1: -2], encoding='utf-8')
+                print(js)
+                insert_item(js)
+            else:
+                print('Error!!! {0}'.format(response.status_code))
 
-        page += 1
+            time.sleep(random.randint(2, 5))
+
+            page += 1
+        except:
+            print('wrong HTTP request...')
 
 
 if __name__ == '__main__':
